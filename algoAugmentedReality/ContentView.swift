2e28e8f76868a53eb7f54d0ca76ee11d109d683d
3dlifeStudio <mjfernandez3d@gmail.com>
2021-03-25 12:39:08 +0000

    Initial Commit

---
+++ algoAugmentedReality/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  algoAugmentedReality
+//
+//  Created by Mario Fernandes on 25/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ algoAugmentedReality/algoAugmentedRealityApp.swift
@@ -0,0 +1,17 @@
+//
+//  algoAugmentedRealityApp.swift
+//  algoAugmentedReality
+//
+//  Created by Mario Fernandes on 25/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct algoAugmentedRealityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
